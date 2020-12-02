d5f315222e7e4b9be84d67ea6a8067c63f85f00c
George <george.pappas@live.com>
2020-12-02 02:27:33 -0600

    created into single file
    took all needed functionality and put it in a single working file

--- public/python/gps_extractor/p-GPSExtractor.py
+++ public/python/gps_extractor/p-GPSExtractor.py
@@ -10,80 +10,176 @@
 
 # Import Declarations.
 import os
-import _modEXIF
-import _csvHandler
-import _commandParser
+from PIL import Image   # import the Python Image Library
+from PIL.ExifTags import TAGS, GPSTAGS  # along with TAGS and GPS related TAGS
 
-# Offsets into the return EXIFData for
-# TimeStamp, Camera Make and Model
-TS = 0
-MAKE = 1
-MODEL = 2
+# Extract EXIF Data
+def ExtractGPSDictionary(fileName):
+    # Open the image.
+    try:
+        pilImage = Image.open(fileName)
+        exifData = pilImage._getexif()
+    except IOError:
+        # If exception occurs from PIL processing
+        print("Failed to open image file {}".format(fileName))
+        return None, None
 
-# Process the Command Line Arguments
-userArgs = _commandParser.ParseCommandLine()
+    # Iterate through the EXIFData
+    # Searching for GPS Tags
+    # Set default values for some image attributes.
+    imageTimeStamp = "NA"
+    CameraModel = "NA"
+    CameraMake = "NA"
 
-csvPath = userArgs.csvPath + "imageResults.csv"
-oCSV = _csvHandler._CSVWriter(csvPath)
+    # Only bother if the image has exif data.
+    if exifData:
+        # Iterate through the values in the exif dictionary.
+        for tag, theValue in exifData.items():
+            tagValue = TAGS.get(tag, tag)   # obtain the tag
 
-# define a directory to scan
-scanDir = userArgs.scanPath
-try:
-    picts = os.listdir(scanDir)
-except:
-    exit(0)
+            # Collect basic image data if available
+            if tagValue == 'DateTimeOriginal':
+                imageTimeStamp = exifData.get(tag)
 
-print("Program Start")
-print()
+            if tagValue == 'Make':
+                cameraMake = exifData.get(tag)
 
-for aFile in picts:
-    targetFile = scanDir + aFile
+            if tagValue == 'Model':
+                cameraModel = exifData.get(tag)
+            
+            # check the tag for GPS
+            if tagValue == "GPSInfo":
+                # Found it !
+                # Now create a Dictionary to hold the GPS Data
+                gpsDictionary = {}
 
-    if os.path.isfile(targetFile):
+                # Loop through the GPS Information
+                for curTag in theValue:
+                    gpsTag = GPSTAGS.get(curTag, curTag)
+                    gpsDictionary[gpsTag] = theValue[curTag]    # Add the text name and value to our dictionary.
 
-        # Update 1.1
-        try:
-            gpsDictionary, exifList = _modEXIF.ExtractGPSDictionary(targetFile)
-        except TypeError:
-            gpsDictionary = None
-            exifList = None
+                basicExifData = [imageTimeStamp, cameraMake, cameraModel]
+                return gpsDictionary, basicExifData
+    else:
+        return None, None
+# End ExtractGPSDictionary ===========================
+
+# Extract the Latitude and Longitude Values
+def ExtractLatLon(gps):
+    if (("GPSLatitude" in gps) and ("GPSLongitude" in gps) and ("GPSLatitudeRef" in gps) and ("GPSLongitudeRef" in gps)):
+        latitude = gps["GPSLatitude"]
+        latitudeRef = gps["GPSLatitudeRef"]
+        longitude = gps["GPSLongitude"]
+        longitudeRef = gps["GPSLongitudeRef"]
+
+        lat = ConvertToDegrees(latitude)
+        lon = ConvertToDegrees(longitude)
+
+        # Check Latitude Reference
+        # If South of the Equator then lat value is negative
+        if latitudeRef == "S":
+            lat = 0 - lat
+
+        # Check Longitude Reference
+        # If West of the Prime Meridian in
+        # Greenwich then the Longitude value is negative
+        if latitudeRef == "W":
+            lat = 0 - lon
+
+        # Update 1.1: Added support for altitude.
+        if (("GPSAltitude" in gps) and ("GPSAltitudeRef" in gps)):
+            altitude = gps['GPSAltitude']
+            altitudeRef = gps['GPSAltitudeRef']
+            
+            gpsCoor = {"Lat": lat, "LatRef":latitudeRef, "Lon": lon, "LonRef": longitudeRef, "Alt": altitude, "AltRef": altitudeRef}
+        else:
+            gpsCoor = {"Lat": lat, "LatRef":latitudeRef, "Lon": lon, "LonRef": longitudeRef, "Alt": '', "AltRef": ''}
         # End Update 1.1 ===========================
 
-        if (gpsDictionary):
-            # Obtain the Lat Lon values from the gpsDictionary
-            # Converted to degrees
-            # The return value is a dictionary key value pairs
-            dCoor = _modEXIF.ExtractLatLon(gpsDictionary)
-
-            lat = dCoor.get("Lat")
-            latRef = dCoor.get("LatRef")
-
-            lon = dCoor.get("Lon")
-            lonRef = dCoor.get("LonRef")
-
-            alt = dCoor.get("Alt")  # Update 1.1
-            altRef = dCoor.get("AltRef")    # Update 1.1
-
-            if (lat and lon and latRef and lonRef):
-                if (alt and altRef):    # Update 1.1
-                    print(aFile+': '+str(lat)+', '+str(lon)+', '+str(alt))
-
-                    # Write one row to the output file.
-                    oCSV.writeCSVRow(targetFile, exifList[TS], exifList[MAKE], exifList[MODEL], latRef, lat, lonRef, lon, altRef, alt)
-                else:
-                    print(aFile+': '+str(lat)+', '+str(lon)+', '+'No ALT Data')
-
-                    # write one row to the output file
-                    oCSV.writeCSVRow(targetFile, exifList[TS], exifList[MAKE], exifList[MODEL], latRef, lat, lonRef, lon, 0, 0)
-            else:
-                print(aFile + ": No GPS EXIF Data") # Update 1.1
-        else:
-            print(aFile + ": No GPS EXIF Data") # Update 1.1
+        return gpsCoor
     else:
-        print(aFile + ": Not a valid file") # Update 1.1
+        return None
+# End Extract Lat Lon ===========================
 
-# Clean up and Close Log and CSV File
-del oCSV
+# Convert GPSCoordinates to Degrees
+def ConvertToDegrees(gpsCoordinate):
+    deg, mins, secs = gpsCoordinate
 
-print("Program End")    # Update 1.1
-print() # Update 1.1
+    try:
+        degrees = float(deg)
+    except:
+        degrees = 0.0
+
+    try:
+        minutes = float(mins)
+    except:
+        minutes = 0.0
+
+    try:
+        seconds = float(secs)
+    except:
+        seconds = 0.0
+
+    floatCoordinate = float(degrees + (minutes / 60.0) + (seconds / 3600.0))
+
+    return floatCoordinate
+
+# Main function.
+if __name__ == '__main__':    
+    # Offsets into the return EXIFData for
+    # TimeStamp, Camera Make and Model
+    TS = 0
+    MAKE = 1
+    MODEL = 2
+
+    print("===== Program Start =====")
+    print()
+
+    targetFile = "IMG_7867 copy.jpeg"
+
+    try:
+        gpsDictionary, exifList = ExtractGPSDictionary(targetFile)
+    except TypeError:
+        gpsDictionary = None
+        exifList = None
+
+    if (gpsDictionary):
+        # Obtain the Lat Lon values from the gpsDictionary
+        # Converted to degrees
+        # The return value is a dictionary key value pairs
+        dCoor = ExtractLatLon(gpsDictionary)
+
+        lat = dCoor.get("Lat")
+        latRef = dCoor.get("LatRef")
+
+        lon = dCoor.get("Lon")
+        lonRef = dCoor.get("LonRef")
+
+        alt = dCoor.get("Alt")
+        altRef = dCoor.get("AltRef")
+
+        if (lat and lon and latRef and lonRef):
+            if (alt and altRef):
+                print(targetFile+':')
+                print('     '+'Latitude:   '+str(latRef)+'  '+str(lat))
+                print('     '+'Longitude:  '+str(lonRef)+'  '+str(lon))
+                print('     '+'Altitude:      '+str(alt))
+            else:
+                print(targetFile+':')
+                print('     '+'Latitude:   '+str(latRef)+'  '+str(lat))
+                print('     '+'Longitude:  '+str(lonRef)+'  '+str(lon))
+                print('     '+'Altitude:      '+'No ALT Data')
+        else:
+            print(targetFile+':')
+            print('     '+'Latitude:  '+'No LAT Data')
+            print('     '+'Longitude: '+'No LON Data')
+            print('     '+'Altitude:  '+'No ALT Data')
+    else:
+        print(targetFile+':')
+        print('     '+'Latitude:  '+'No LAT Data')
+        print('     '+'Longitude: '+'No LON Data')
+        print('     '+'Altitude:  '+'No ALT Data')
+
+    print()
+    print("===== Program End =====")
+    print()
