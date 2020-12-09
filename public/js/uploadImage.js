80801ff6a5715bf313263eeb94acc3601e7071a7
George <george.pappas@live.com>
2020-12-09 02:52:17 -0600

    Create uploadImage.js

---
+++ public/js/uploadImage.js
@@ -0,0 +1,20 @@
+function printEXIF(filename) {
+
+    // document.forms[filename].action='action="https://albert-upload-image.azurewebsites.net/api/Upload-User-Image?code=y6cWO/8xzFfJqBXgg47QTMmm2iK/xJ37pV68xVhIaBxpvhDT9J536g=="';
+    // document.forms[filename].method='POST';
+    // document.forms[filename].target='_blank';
+    // document.forms[filename].enctype="multipart/form-data";
+    // document.forms[filename].submit();
+
+
+    alert(filename.value)
+    //var txt = 'https://albert-final-project-function.azurewebsites.net/api/gps_extractor?'
+    //var obj = JSON.parse(txt);
+
+    //alert(obj);
+
+    //document.getElementById("date").innerHTML = obj.date;
+    //document.getElementById("latitude").innerHTML = obj.latRef + " " + obj.lat;
+    //document.getElementById("longitude").innerHTML = obj.lonRef + " " + obj.lon;
+    //document.getElementById("altitude").innerHTML = obj.alt;
+}
