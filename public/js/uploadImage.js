337f980af1299a9dfd44b358a0589de0f0a72b33
lpetrongellijr <lpetrongellijr@yahoo.com>
2020-12-10 15:26:07 -0600

    more failed
    cant figure out the uploading. This gives me a type error so maybe it has something to do with the server side

--- public/js/uploadImage.js
+++ public/js/uploadImage.js
@@ -1,5 +1,52 @@
 function uploadImage() {
-    alert('image uploaded')
+
+    var input = document.querySelector('input[type="file"]')
+    alert(input.files)
+
+    var data = new FormData()
+    data.append('file', input.files[0])
+    // data.append('user', 'hubot')
+    fetch('https://albert-upload-image.azurewebsites.net/api/Upload-User-Image?code=y6cWO/8xzFfJqBXgg47QTMmm2iK/xJ37pV68xVhIaBxpvhDT9J536g==', {
+        method: 'POST',
+        body: data,
+    }).then(
+        response => response.json(), // if the response is a JSON object
+    ).then(
+        success => alert(success), // Handle the success response object
+    ).catch(
+        error => alert(error), // Handle the error response object  
+    );
+
+
+
+
+    // // Select your input type file and store it in a variable
+    // const input = document.getElementById('filename');
+    
+    // // This will upload the file after having read it
+    // const upload = (file) => {
+    //     fetch('https://albert-upload-image.azurewebsites.net/api/Upload-User-Image?code=y6cWO/8xzFfJqBXgg47QTMmm2iK/xJ37pV68xVhIaBxpvhDT9J536g==', { // Your POST endpoint
+    //         method: 'POST',
+    //         body: file // This is your file object
+    //     }).then(
+    //         alert('hi'),
+    //         response => response.json() // if the response is a JSON object
+    //     ).then(
+    //         alert('image uploaded'),
+    //         success => console.log(success) // Handle the success response object
+    //     ).catch(
+    //         alert('failed'),
+    //         error => console.log(error) // Handle the error response object 
+    //     );
+    // };
+    // // Event handler executed when a file is selected
+    // const onSelectFile = () => upload(input.files[0]);
+
+    // // Add a listener on your input
+    // // It will be triggered when a file will be selected
+    // input.addEventListener('change', onSelectFile, false);
+
+
 
     // FAILED
     // var form = document.createElement("form"); 
